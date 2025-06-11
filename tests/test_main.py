5cc8aee4e54ddd6bb252a32e5f9a23aab14446c0
Dan Mendoza <daniel.mendoza@forsta.com>
2025-06-11 12:19:29 -0600

    Force a failyre

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,4 +7,4 @@ async def test_read_root(async_client: AsyncClient) -> None:
     """Test the root endpoint returns the expected greeting."""
     response = await async_client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"Hello": "Welcome to the 211."}
+    assert response.json() == {"Hello": "Welcome to the 211. Fail"}
