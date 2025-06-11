2989dac9b7e19b80e70eccfbc3f40a8c290d9d70
Dan Mendoza <daniel.mendoza@forsta.com>
2025-06-11 10:31:03 -0600

    FSAR-83: Linting with black.

--- app/main.py
+++ app/main.py
@@ -1,29 +1,23 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import APIRouter, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from user.routes.user import router as user_router
-from db import Base, engine
 
 app = FastAPI()
 v1_router = APIRouter(prefix="/api/v1")
 v1_router.include_router(user_router, prefix="/user")
 app.include_router(v1_router)
 
-origins = [
-    "https://localhost:5173",
-    "http://localhost:5173",
-    "localhost:5173"
-]
+origins = ["https://localhost:5173", "http://localhost:5173", "localhost:5173"]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
+
 
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
-    return {
-        "Hello": "Welcome to the 211 some chagne in the code, this was auto deployed on merge"
-    }
+    return {"Hello": "Welcome to the 211."}
